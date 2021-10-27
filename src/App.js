1511bd33843171fd3793f98c176ce08c7ad6f753
Dustin Welch <dustinwwelch1@gmail.com>
2021-10-26 21:52:17 -0500

    basic to-do app done, want to event handle enter key still

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from './TodoList.js'
-
 const { v4: uuidv4 } = require('uuid');
 // import uuidv4 from 'uuid/v4'
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])  
   const todoNameRef = useRef()
+
+  // store the todos
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  // get the todos
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
@@ -20,11 +46,12 @@ function App() {
 
   return (
     <>
-      <TodoList todos={todos} />
-      <input ref={todoNameRef} type="text" />
+      <div>DW's To-do List</div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" onKeyPress="if(event.keyCode == 13){handleAddTodo}"/>
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Complete</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodos}>Clear Complete</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   )
 }
